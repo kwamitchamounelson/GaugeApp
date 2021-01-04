56a128b1a6fa3d363a1a76b7990aad618f15b1a9
kwamitchamounelson <kwamitchamounelson@gmail.com>
2021-01-04 13:11:52 +0000

    done deb to receive com loan

--- app/src/main/java/com/example/gaugeapp/ui/communityLoan/debToReceive/DebToReceiveFragment.kt
+++ app/src/main/java/com/example/gaugeapp/ui/communityLoan/debToReceive/DebToReceiveFragment.kt
@@ -1,12 +1,19 @@
 package com.example.gaugeapp.ui.communityLoan.debToReceive
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.gaugeapp.R
+import com.example.gaugeapp.ui.communityLoan.items.DebToReceiveItem
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Section
+import com.xwray.groupie.kotlinandroidextensions.ViewHolder
+import kotlinx.android.synthetic.main.deb_to_receive_fragment.*
+import kotlinx.android.synthetic.main.deb_to_receive_fragment.view.*
 
 class DebToReceiveFragment : Fragment() {
 
@@ -20,13 +27,28 @@ class DebToReceiveFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.deb_to_receive_fragment, container, false)
+        val root = inflater.inflate(R.layout.deb_to_receive_fragment, container, false)
+        (requireActivity() as AppCompatActivity).setSupportActionBar(root.my_toolbar_deb_to_receive)
+        setHasOptionsMenu(true)
+        return root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(DebToReceiveViewModel::class.java)
-        // TODO: Use the ViewModel
+        updateUi()
+    }
+
+    private fun updateUi() {
+        val items = (0..10).map {
+            DebToReceiveItem()
+        }
+
+        id_list_deb_to_receive_rv.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = GroupAdapter<ViewHolder>().apply {
+                add(Section(items))
+            }
+        }
     }
 
 }
