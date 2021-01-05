58e62d43b19a58b4510d05ec90056e3f224d0a81
kwamitchamounelson <kwamitchamounelson@gmail.com>
2021-01-05 13:28:27 +0000

    done about you view

--- app/src/main/java/com/example/gaugeapp/ui/communityLoan/yourProfile/aboutYou/AboutYouFragment.kt
+++ app/src/main/java/com/example/gaugeapp/ui/communityLoan/yourProfile/aboutYou/AboutYouFragment.kt
@@ -1,12 +1,13 @@
 package com.example.gaugeapp.ui.communityLoan.yourProfile.aboutYou
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.gaugeapp.R
+import kotlinx.android.synthetic.main.about_you_fragment.*
 
 class AboutYouFragment : Fragment() {
 
@@ -25,8 +26,19 @@ class AboutYouFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AboutYouViewModel::class.java)
-        // TODO: Use the ViewModel
+        updateUI()
+    }
+
+    private fun updateUI() {
+        val image =
+            "https://firebasestorage.googleapis.com/v0/b/kola-application.appspot.com/o/USER_PROFILS%2F3hV4TKs15ffQ8MeIT0lPV9Ao2vl1?alt=media&token=ccb7acb8-f7df-4ca0-bcf8-f50e29b9c475"
+
+        Glide.with(this)
+            .load(image)
+            .placeholder(R.drawable.ic_person_black_24dp)
+            .error(R.drawable.ic_person_black_24dp)
+            .circleCrop()
+            .into(id_your_profile_user_profile)
     }
 
 }
